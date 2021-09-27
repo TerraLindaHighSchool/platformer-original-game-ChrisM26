9b1e2daa1b91cee50c08f541f493381e25c5121c
Chris <cm01404@srcs.org>
2021-09-27 17:48:06 -0500

    finished for monday
    updated

--- BrickWall.java
+++ BrickWall.java
@@ -1,20 +1,19 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Bomb here.
+ * Write a description of class BrickWall here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Bomb extends Obstacle
+public class BrickWall extends Platform
 {
     /**
-     * Act - do whatever the Bomb wants to do. This method is called whenever
+     * Act - do whatever the BrickWall wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
-        private float yVelocity;
-        private final float GRAVITY;
+        // Add your action code here.
     }
 }
--- Level1.java
+++ Level1.java
@@ -28,6 +28,23 @@ public class Level1 extends World
         Player player = new Player();
         addObject(player,68,740);
         setPaintOrder(Player.class, Platform.class, Obstacle.class, Collectable.class,
-              Door.class, HUD.class);
+            Door.class, HUD.class);
+        Door door= new Door();
+        addObject(door,1171,44);
+        addObject(player,96,750);
+        addObject(new Floor(), 600, 800);
+        addObject(new BrickWall(), 380, 500);
+        addObject(new BrickWall(), 780, 300);
+        addObject(new BrickWall(), 960, 100);
+        addObject(new SmBrickWall(), 1120, 600);
+        addObject(new SmBrickWall(), 880, 600);
+        addObject(new SmBrickWall(), 420, 160);
+        addObject(new SmBrickWall(), 1000, 600);
+        addObject(new SmBrickWall(), 220, 280);
+        addObject(new TrapDoor(), 1000, 600);
+        addObject(new Bomb(), 455, 114);
+        addObject(new Bomb(), 1060, 544);
+        addObject(new Gem(),975,160);
+        addObject(new Gem(), 1030, 160);
     }
 }
